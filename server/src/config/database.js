19e890d8a8680ce0570d86679a63edec522d7649
Arunabha Banerjee <arunabhabanerjee5@gmail.com>
2025-12-19 19:24:43 +0530

    updated code

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://HRSphere:9PEBE7vsX9JbruG@cluster0.ihcx9ce.mongodb.net/devTinder"
-  );
+  await mongoose.connect(process.env.MONGODB_URI);
 };
     
 module.exports = connectDB;
